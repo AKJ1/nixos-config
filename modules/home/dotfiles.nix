4e393d6a8409d44d9049271a7424a0f212c50957
Ace <git@zhechev.net>
2025-11-26 04:20:52 +0200

    comment out configfiles until i get some

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -3,17 +3,17 @@
 let
   # All entries in ./dotfiles (only directories usually matter)
   homefiles = builtins.readDir ./dotfiles/home;
-  configfiles = builtins.readDir ./dotfiles/config;
+  # configfiles = builtins.readDir ./dotfiles/config;
 in
 {
   xdg.enable = true;
 
   # Create an xdg.configFile entry for each directory/file in ./dotfiles
-  xdg.home = lib.genAttrs (builtins.attrNames configfiles) (name: {
-    source = ./dotfiles/config/${name};
-    recursive = true;
-    # No 'force' or immutability — symlink is writable because the target is.
-  });
+  # xdg.home = lib.genAttrs (builtins.attrNames configfiles) (name: {
+  #   source = ./dotfiles/config/${name};
+  #   recursive = true;
+  #   # No 'force' or immutability — symlink is writable because the target is.
+  # });
 
   home.file = lib.genAttrs (builtins.attrNames homefiles) (name: {
     source = ./dotfiles/home/${name};
