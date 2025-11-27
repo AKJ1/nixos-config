3c7668210661795c8ef80104aa38138390e8e510
Ace <git@zhechev.net>
2025-11-27 02:15:19 +0200

    mess with nv configuration

--- modules/home/nvidia.nix
+++ modules/home/nvidia.nix
@@ -12,16 +12,4 @@
     nvtopPackages.nvidia
   ];
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    nvidiaSettings = true;
-    open = false;
-
-    prime = {
-      offload.enable = true;
-      intelBusId = "PCI:0:2:0"; # example, run `lspci`
-      nvidiaBusId = "PCI:1:0:0";
-    };
-  };
 }
