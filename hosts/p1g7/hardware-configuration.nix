337cd78e0bb67505b7891da0181a556e35baabc8
Ace <git@zhechev.net>
2025-11-27 02:15:11 +0200

    mess with nv configuration

--- hosts/p1g7/hardware-configuration.nix
+++ hosts/p1g7/hardware-configuration.nix
@@ -59,4 +59,17 @@
   hardware.graphics = {
     enable = true;
   };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    nvidiaSettings = true;
+    open = false;
+
+    prime = {
+      offload.enable = true;
+      intelBusId = "PCI:0:2:0"; # example, run `lspci`
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
 }
