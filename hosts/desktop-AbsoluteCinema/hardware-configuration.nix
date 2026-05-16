0aa012395e0fab91fb6f54a59a22f3c8d0a63c85
ace <ace@ace.ac>
2026-05-16 04:17:14 +0300

    fix boot error

--- hosts/desktop-AbsoluteCinema/hardware-configuration.nix
+++ hosts/desktop-AbsoluteCinema/hardware-configuration.nix
@@ -16,7 +16,14 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ahci"
+    "nvme"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [
     "kvm-intel"
@@ -24,25 +31,32 @@
   ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/bf56e52e-e234-4c0f-b2b6-97e5d0aea195";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/bf56e52e-e234-4c0f-b2b6-97e5d0aea195";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F7AD-FD2D";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/3aadc27e-e412-453c-80bc-02ce43f39cb4";
-      fsType = "ext4";
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/35170f36-6870-415b-8a32-3f6c69706371"; }
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/F7AD-FD2D";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
     ];
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/3aadc27e-e412-453c-80bc-02ce43f39cb4";
+    fsType = "ext4";
+  };
+  fileSystems."/home/ace/FATGaming" = {
+    device = "/dev/disk/by-uuid/D468-40E0";
+    fsType = "vfat";
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/35170f36-6870-415b-8a32-3f6c69706371"; }
+  ];
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
@@ -58,17 +72,17 @@
   };
 
   hardware.nvidia = {
-     package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
-     modesetting.enable = true;
-     powerManagement.enable = false;
-     nvidiaSettings = true;
-     open = false;
-#
-#     prime = {
-#       offload.enable = false;
-#       intelBusId = "PCI:0:2:0"; # example, run `lspci`
-#       nvidiaBusId = "PCI:1:0:0";
-#     };
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    nvidiaSettings = true;
+    open = false;
+    #
+    #     prime = {
+    #       offload.enable = false;
+    #       intelBusId = "PCI:0:2:0"; # example, run `lspci`
+    #       nvidiaBusId = "PCI:1:0:0";
+    #     };
   };
   services.xserver.videoDrivers = [
     "nvidia"
