921699cb2814aefce7e1213cbc2973c8610db55e
ace <ace@ace.ac>
2026-05-16 12:52:52 +0300

    enable mouse actions on AbsoluteCinema

--- hosts/desktop-AbsoluteCinema/default.nix
+++ hosts/desktop-AbsoluteCinema/default.nix
@@ -5,6 +5,7 @@
     ./../../modules/core
     ./../../modules/core/optional/bluetooth.nix
     ./../../modules/core/optional/printers.nix
+    ./../../modules/core/optional/mouse-actions.nix
   ];
   _module.args.displayConfig = "1.25";
 
@@ -17,16 +18,15 @@
 
   services = {
 
-
     tlp.settings = {
-        # High performance when on AC
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_BOOST_ON_AC = 1;
-        CPU_HWP_DYN_BOOST_ON_AC = 1;
-        PLATFORM_PROFILE_ON_AC = "performance";
-        INTEL_GPU_MIN_FREQ_ON_AC = 1500; # Higher for performance
-        INTEL_GPU_MAX_FREQ_ON_AC = 0; # Use maximum (auto)
-        INTEL_GPU_BOOST_FREQ_ON_AC = 0; # Use maximum (auto)
+      # High performance when on AC
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      PLATFORM_PROFILE_ON_AC = "performance";
+      INTEL_GPU_MIN_FREQ_ON_AC = 1500; # Higher for performance
+      INTEL_GPU_MAX_FREQ_ON_AC = 0; # Use maximum (auto)
+      INTEL_GPU_BOOST_FREQ_ON_AC = 0; # Use maximum (auto)
     };
   };
 
