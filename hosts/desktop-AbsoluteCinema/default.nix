7728dc5e8c65dc1b4a781396bfcd7beb909f07a5
ace <ace@ace.ac>
2026-05-28 18:08:58 +0300

    enable amd on abs.cinema

--- hosts/desktop-AbsoluteCinema/default.nix
+++ hosts/desktop-AbsoluteCinema/default.nix
@@ -5,6 +5,7 @@
     ./../../modules/core
     ./../../modules/core/optional/bluetooth.nix
     ./../../modules/core/optional/printers.nix
+    ./../../modules/core/optional/amd.nix
     # ./../../modules/core/optional/mouse-actions.nix
   ];
   _module.args.displayConfig = "1.25";
