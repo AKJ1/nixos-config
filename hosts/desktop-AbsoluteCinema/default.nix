faf1662e21aa592b4fb05f5950e546c5e61812af
ace <ace@ace.ac>
2026-06-04 03:05:18 +0300

    add sops to absolutecinema

--- hosts/desktop-AbsoluteCinema/default.nix
+++ hosts/desktop-AbsoluteCinema/default.nix
@@ -7,6 +7,7 @@
     ./../../modules/core/optional/printers.nix
     ./../../modules/core/optional/amd.nix
     ./../../modules/core/optional/docker.nix
+    ./../../modules/core/optional/sops.nix
     # ./../../modules/core/optional/mouse-actions.nix
   ];
   _module.args.displayConfig = "1.25";
